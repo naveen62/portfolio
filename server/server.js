d3970a087136c398166f38a4301b286bbabe4c8a
naveen62 <coolnaveen62@gmail.com>
2019-04-28 16:25:04 +0530

    added Port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require("express");
+const PORT = process.env.PORT || 5000
 
 const app = express();
 const publicDir = path.join(__dirname, '../public')
 
 app.use(express.static(publicDir));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('server is running')
 })
